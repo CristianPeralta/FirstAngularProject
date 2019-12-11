d70ef4d9d850155a327b4d2312b6a684bcde0b1c
CristianPeralta <cbperaltasegura@gmail.com>
2019-12-11 15:59:05 -0500

    add product-alerts-component component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponentComponent } from './product-alerts-component/product-alerts-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductAlertsComponentComponent
   ],
   imports: [
     BrowserModule,
